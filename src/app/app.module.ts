3c30afae9e5bcce7904e907d5a4cbec93901c43c
Xavier Georget <xageorget@gmail.com>
2019-04-10 15:45:32 +0200

    add of planet.ts, planet.component, planet.service, planet-system.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlanetComponent } from './planet/planet.component';
+import { PlanetSystemComponent } from './planet-system/planet-system.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlanetComponent,
+    PlanetSystemComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/planet-system/planet-system.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-planet-system',
+  templateUrl: './planet-system.component.html',
+  styleUrls: ['./planet-system.component.css']
+})
+export class PlanetSystemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
