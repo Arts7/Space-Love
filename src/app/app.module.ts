ab9cad82d133d072a958831caa959f3f0df4b496
Lyneeee <namiss.lyne@gmail.com>
2019-04-24 10:43:50 +0200

    envoi du logo & menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { HelpComponent } from './help/help.component';
+import { SpaceMapComponent } from './space-map/space-map.component';
+import { HomeComponent } from './home/home.component';
+import { YourProfileComponent } from './your-profile/your-profile.component';
+import {ROUTES} from './app.routes';
+import { LogoComponent } from './logo/logo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HelpComponent,
+    SpaceMapComponent,
+    HomeComponent,
+    YourProfileComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/space-map/space-map.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-space-map',
+  templateUrl: './space-map.component.html',
+  styleUrls: ['./space-map.component.css']
+})
+export class SpaceMapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
