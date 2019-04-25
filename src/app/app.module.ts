26cdc6fe9c842f0402d3200764fd3e10a65c4e7c
Lyneeee <namiss.lyne@gmail.com>
2019-04-25 17:41:41 +0200

    envoi mise en page 4ème page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -27,7 +28,8 @@ import { DescriptionsComponent } from './descriptions/descriptions.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(ROUTES)
+    RouterModule.forRoot(ROUTES),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/person.ts
@@ -0,0 +1,10 @@
+export class Person {
+    public id:number;
+    public name:string;
+    public sexe:string;
+    public nberEyes:number;
+    public typeSkin:string;    
+    public job:string;
+    public description:string;
+    public photos:string[];
+}
