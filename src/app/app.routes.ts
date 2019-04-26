3a1eee3f8ea81a3d9eacb105915eab24ad89339c
Arthur Jouanneau <jouanneauarthur@gmail.com>
2019-04-26 10:13:01 +0200

    modification des routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,14 @@ import{YourProfileComponent} from './your-profile/your-profile.component';
 import{HomeComponent} from './home/home.component';
 import{SpaceMapComponent} from './space-map/space-map.component';
 import{HelpComponent} from './help/help.component';
+import {PlanetComponent} from './planet/planet.component';
 
 
 
 const ROUTES: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
-  { path: 'space-map', component: SpaceMapComponent },
+  { path: 'planet', component: PlanetComponent },
   { path: 'your-profile', component: YourProfileComponent },
   { path: 'help', component: HelpComponent },
 
